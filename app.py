2cf76c6552f0917dfe51433fc2b5bff46679a86e
rauf8888 <mrauf2907@gmail.com>
2024-03-01 00:58:00 +0530

    Files Added: More Templates with Modifications in app.py File

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, request, render_template, url_for,flash
+from flask import Flask, request, render_template, url_for,flash,redirect
+import datetime
 import sqlite3
 
 app = Flask(__name__)
 app.secret_key = "ThisIsASecretKey@1234"
-
 data_dict = {"rauf":"12345","Dharshan":'67890'}
+
+
 
 @app.route('/home')
 def landing_page(): 
@@ -16,7 +18,10 @@ def doctor_login():
         duser = request.form['username']
         dpass = request.form['password']
 
-        print(duser,dpass)
+        if duser in data_dict and data_dict[duser] == dpass:
+            return redirect(url_for('doctor_dashboard'))
+        else:
+            flash("Invalid Username or Password")
 
     return render_template('doctor_login.html')
 
@@ -26,9 +31,21 @@ def patient_login():
         puser = request.form['username']
         ppass = request.form['password']
 
-        print(puser,ppass)   
+        if puser in data_dict and data_dict[puser] == ppass:
+            return redirect(url_for('patient_dashboard'))
+        else:
+            flash("Incorrect Username or Password")
  
     return render_template('patient_login.html')
+
+@app.route('/doctor_dashboard',methods = ['GET','POST'])
+def doctor_dashboard():
+    return render_template('doctor_form.html')
+
+@app.route('/patient-dashboard')
+def patient_dashboard():
+    return "Hey there, Patient"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
