34b7ff64670ac2e790ba27dc9635092186da8403
rauf8888 <mrauf2907@gmail.com>
2024-02-29 23:08:40 +0530

    Files Added & Modified: Redirecting to Login Pages

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, url_for,flash
 import sqlite3
 
 app = Flask(__name__)
 app.secret_key = "ThisIsASecretKey@1234"
 
-@app.route('/')
+data_dict = {"rauf":"12345","Dharshan":'67890'}
+
+@app.route('/home')
 def landing_page(): 
     return render_template('landing.html')
+
+@app.route('/doctor-login',methods = ['GET','POST'])
+def doctor_login():
+    if request.method == 'POST':
+        duser = request.form['username']
+        dpass = request.form['password']
+
+        print(duser,dpass)
+
+    return render_template('doctor_login.html')
+
+@app.route('/patient-login',methods = ['GET','POST'])
+def patient_login(): 
+    if request.method == 'POST':
+        puser = request.form['username']
+        ppass = request.form['password']
+
+        print(puser,ppass)   
+ 
+    return render_template('patient_login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
